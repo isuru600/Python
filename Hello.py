d63fe36c72696ee2f1b382b41772c58b86b98662
isuru600 <isuruliyanage600@gmail.com>
2026-01-16 10:59:33 +0530

    Intial Commit

---
+++ Hello.py
@@ -0,0 +1,15 @@
+def print_greeting():
+    print("Hello Python")
+
+def print_details():
+    name = "John"
+    role = "Beginner Python Learner"
+    year = 2026
+
+    print("Name:", name)
+    print("Role:", role)
+    print("Year:", year)
+
+# Main execution
+print_greeting()
+print_details()
